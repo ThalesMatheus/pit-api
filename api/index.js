b8fd49184d802a019de7d81ad17327e29b765e5f
ThalesMatheus <fvjkzw@gmail.com>
2023-06-06 11:16:40 -0300

    PSR JAVASCRIPT

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-import express from "express"
-import userRoutes from "./routes/users.js"
-import cors from "cors"
-import dotenv from 'dotenv';
-import fileUpload from "express-fileupload";
+import express from 'express'
+import userRoutes from './routes/users.js'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import fileUpload from 'express-fileupload'
 dotenv.config()
 const app = express()
 app.use(fileUpload())
 app.use(express.json())
 app.use(cors())
 
-app.use("/", userRoutes)
+app.use('/', userRoutes)
 
 app.listen(8800)
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,20 +1,20 @@
-import express from "express";
-import { addUser, deleteUser, getUser, updateUser, autUser, validaCookie, auth_check, updateImage } from "../controllers/user.js";
+import express from 'express'
+import { addUser, deleteUser, getUser, updateUser, autUser, validaCookie, auth_check, updateImage } from '../controllers/user.js'
 
 const router = express.Router()
 
-router.get("/", getUser)
+router.get('/', getUser)
 
-router.post("/register", addUser)
+router.post('/register', addUser)
 
-router.post("/login", autUser)
-router.post("/valida", validaCookie)
-router.post("/auth_check", auth_check)
-router.post("/update", updateImage)
-//router.post("/auth" authUser)
+router.post('/login', autUser)
+router.post('/valida', validaCookie)
+router.post('/auth_check', auth_check)
+router.post('/update', updateImage)
+// router.post("/auth" authUser)
 
-router.put("/:id", updateUser)
+router.put('/:id', updateUser)
 
-router.delete("/:id", deleteUser)
+router.delete('/:id', deleteUser)
 
 export default router
