33b0736f5ecf8b63d4370814af870fa45ea92ff9
ThalesMatheus <fvjkzw@gmail.com>
2023-09-10 14:55:15 -0300

    Hotfix: merge 	modified:   api/controllers/user.js 	modified:   api/package.json 	modified:   api/routes/users.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -131,7 +131,7 @@ export const updateUser = (req, res) => {
 }
 
 export const deleteUser = (req, res) => {
-  const q = 'DELETE FROM login WHERE `id` = ?'
+  const q = 'DELETE FROM login WHERE `uuid` = ?'
 
   db.query(q, [req.params.id], (err) => {
     if (err) return res.json(err)
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { addUser, deleteUser, getUser, updateUser, autUser, validaCookie, auth_check, updateImage, forgotPass } from '../controllers/user.js'
+import { addUser, deleteUser, getUser, updateUser, deleteGroup, createGroup, autUser, validaCookie, auth_check, forgotPass, getAllGroups } from '../controllers/user.js'
+import { updateImage } from '../controllers/updateImage.js'
 
 const router = express.Router()
 
@@ -11,10 +12,9 @@ router.post('/login', autUser)
 router.post('/valida', validaCookie)
 router.post('/auth_check', auth_check)
 router.post('/update', updateImage)
-// router.post("/auth" authUser)
-
+router.get('/groups', getAllGroups)
 router.put('/:id', updateUser)
-
+// router.post("/auth" authUser)
 router.delete('/:id', deleteUser)
 
 export default router
