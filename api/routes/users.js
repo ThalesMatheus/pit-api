32d11eb604f0890726bbabbad8cbef03850023af
Thales Matheus <73052784+ThalesMatheus@users.noreply.github.com>
2023-09-10 15:56:21 -0300

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -12,8 +12,11 @@ router.post('/login', autUser)
 router.post('/valida', validaCookie)
 router.post('/auth_check', auth_check)
 router.post('/update', updateImage)
-router.get('/groups', getAllGroups)
 router.put('/:id', updateUser)
+router.post('/creategroup', createGroup)
+router.post('/groups/delete', deleteGroup)
+
+router.get('/groups', getAllGroups)
 // router.post("/auth" authUser)
 router.delete('/:id', deleteUser)
 
